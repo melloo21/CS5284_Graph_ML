9f7d1cdc6e3cbfce358464c5e76c975531b585f9
melissa <dhytra97@gmail.com>
2024-10-12 18:08:06 +0800

    adding all the scrapping

--- utils/wiki_scrapper.py
+++ utils/wiki_scrapper.py
@@ -1,6 +1,23 @@
 import re
 import wptools
 import wikipediaapi
+import sys
+import os
+
+# Suppress output by redirecting sys.stdout temporarily
+# Suppress both stdout and stderr by redirecting them to os.devnull
+class SuppressOutput:
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        self._original_stderr = sys.stderr
+        sys.stdout = open(os.devnull, 'w')
+        sys.stderr = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stderr.close()
+        sys.stdout = self._original_stdout
+        sys.stderr = self._original_stderr
 
 class GenericScrapper:
 
@@ -12,9 +29,27 @@ class GenericScrapper:
         print("Page - Title: %s" % page.title)
         print("Page - URL: %s" % page.fullurl)
 
+    def _print_sections(self, sections, level=0):
+        for s in sections:
+            print("%s: %s" % ("*" * (level + 1), s.title))
+            self._print_sections(s.sections, level + 1)
+
     def _filter_year(self):
         return
-        
+
+    ## Using WikiAPI
+    def get_page_sections(self, page_title:str):
+        try:
+            self._print_sections(self.wiki_wiki.page(page_title).sections)
+        except Exception as e:
+            print(f" ERROR [get_page_sections] :: {e}")
+
+    def get_section_dict(self, page_title:str):
+        try:    
+            return {elem.title: elem.text for elem in self.wiki_wiki.page(page_title).sections}
+        except Exception as e:
+            print(f" ERROR [get_section_dict] :: {e}")
+
     def get_page_summary(self, page_title:str):
         try:
             if page.exists():
@@ -28,28 +63,35 @@ class GenericScrapper:
             print(f" ERROR [get_page_summary] :: {e}")
 
     def _getting_page_info(self, page_title:str):
-        # Getting the page
-        page = wptools.page(page_title)
-        page.get()
-        
-        released_info = page.data["infobox"]["released"]
-        return page, released_info
+        try:
+            with SuppressOutput():
+                # Getting the page
+                page = wptools.page(page_title)
+                page.get()
+            
+            released_info = page.data["infobox"]["released"]
+            return page, released_info
+        except Exception as e:
+            print(f" ERROR [getting_page_info] :: {e}")
 
+    ## Using WikiMediaAPI (wptools)
     def get_page_summary_w_release_date(self, page_title:str, date:str):
+        """
+         Returns the release date of movie
+         Returns extracted date
+        """
         try:
             # Get page details
-            page, released_info =  self._getting_page_info()
+            page, released_info =  self._getting_page_info(page_title)
+
+            # Format release info
+            year_matches = re.search(r'\b(\d{4})\b', released_info)
 
             if date in released_info:
-                print(f" {page_title} :: {released_info} Found ")
+                print(f" {page_title} {year_matches.group(1)} :: {released_info} Found ")
                 return re.sub("\n", "" ,re.sub(self.remove_html, '', page.data['extract']))
             else:
                 print(f" {page_title} :: {released_info} Not Found ")
 
         except Exception as e:
             print(f" ERROR [get_page_summary_w_release_date] :: {e}")
-
-    def get_plot(self, page_title:str, date:str):
-        # Get page details
-        page, released_info =  self._getting_page_info()
-        return
